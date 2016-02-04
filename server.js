efdf9af023a509b2fa076450492c9bed504b1ead
Dave Manley <manley@stanfordalumni.org>
2016-02-04 03:09:53 +0000

    remove some clementine stuff for test

--- server.js
+++ server.js
@@ -3,12 +3,10 @@
 var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
-var passport = require('passport');
 var session = require('express-session');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -16,16 +14,6 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {

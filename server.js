dba31180f5e069a43e22fb20396285cfe10f0f00
Dave Manley <manley@stanfordalumni.org>
2016-02-05 04:27:05 +0000

    add basic db stuff

--- server.js
+++ server.js
@@ -11,10 +11,54 @@ require('dotenv').load();
 
 mongoose.connect(process.env.MONGO_URI);
 
+var ServiceProviderSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    foodType:String,
+    phone:String,
+    addr:String,
+    menu:String,
+//    lat:Double,
+  //  lon:Double,
+    //averageUserRating:Double,
+//    created:Timestamp,
+  //  lastupdated:Timestamp
+});
+
+let sModel = mongoose.model('SearchList', ServiceProviderSchema);
+
+
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
+
+app.get('/get-vendors/:filter', function(req, res) {
+   let filter = req.params.filter;
+ 
+    let q = sModel.find({}, {
+        _id: 0,
+        __v: 0
+    });
+    q.exec(function(err, data) {
+        if (err) console.log("please send a mail to manley@stanford.org, he will be interested in the error..");
+        res.end(JSON.stringify(data));
+    });
+});
+
+
+
+app.get('/add-vendor/:str', function(req, res) {
+    let str = req.params.str;
+    let fields = str.split("?");
+    let newVendor = new sModel({
+        firstName: fields[0],
+    });
+    newVendor.save();
+
+    let retval = { "status:":fields[0] };
+    res.send(JSON.stringify(retval));
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
